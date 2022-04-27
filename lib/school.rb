653d49c6ca9d071db0bf98f0271d446554899ad3
Janan Akkad <jananakkad@hotmail.co.uk>
2022-04-27 11:55:59 +0100

    Completed assignment

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,42 @@
-# code here!
+class School
+
+    def initialize(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student_name, grade)
+        @student_name = student_name
+        @grade = grade
+
+        if roster.include?(grade) == false
+            @roster[grade] = []
+        end
+        @roster[grade] << student_name
+
+        #or
+        # roster[grade] ||= []      i.e. if roster[grade] == nil or falsee, then set it equal to an empty array
+		# roster[grade] << student
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, student_name|
+            @roster[grade] = student_name.sort
+        end
+    end
+end
